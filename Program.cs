831b6326deecb3ad23959edfaf92390f663b22de
ahamm <ahammer753@gmail.com>
2023-09-05 21:16:41 -0500

    added endpoints for song

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Mvc;
 using TunaPiano;
+using TunaPiano.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +20,7 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddNpgsql<TunaPianoDbContext>(builder.Configuration["TunaPianoDbConnectionString"]);
 
 // Set the JSON serializer options
-builder.Services.Configure<JsonOptions>(options =>
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
 {
     options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
@@ -37,5 +39,54 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/api/song", (TunaPianoDbContext db) =>
+{
+    return db.Song.ToList();
+});
+
+app.MapPost("/api/song", (TunaPianoDbContext db, [FromBody] Song song) =>
+{
+    db.Song.Add(song);
+    db.SaveChanges();
+
+    return Results.Created($"/api/song/{song.Songid}", song);
+});
+
+app.MapDelete("/api/song/{Id}", (int sId, TunaPianoDbContext db) =>
+{
+    Song s = db.Song.FirstOrDefault(p => p.Songid == sId);
+
+    if (s == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Song.Remove(s);
+    db.SaveChanges();
+
+    return Results.Ok(s);
+});
+
+app.MapPut("/api/songs/{songId}", (int songId, [FromBody] Song updatedSong, TunaPianoDbContext db) =>
+{
+    Song existingSong = db.Song.FirstOrDefault(s => s.Songid == songId);
+
+    if (existingSong == null)
+    {
+        return Results.NotFound();
+    }
+
+    existingSong.Title = updatedSong.Title;
+    existingSong.ArtistId = updatedSong.ArtistId;
+    existingSong.Album = updatedSong.Album;
+    existingSong.length= updatedSong.length;
+    existingSong.Genre = updatedSong.Genre;
+
+    db.SaveChanges();
+
+    return Results.Ok(existingSong);
+});
+
 
 app.Run();
